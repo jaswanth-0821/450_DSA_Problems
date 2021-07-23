ecf80d177c7b68af197a875786e24c8f9c72475f
JASWANTH REDDY .P <77910115+jaswanth-0821@users.noreply.github.com>
2021-07-23 18:10:02 +0530

    Create union_of_two_array.cpp

---
+++ Array/union_of_two_array.cpp
@@ -0,0 +1,46 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include<set>
+class Solution{
+    public:
+    int doUnion(int a[], int n, int b[], int m)  
+    {
+        
+        set<int>s1;
+        for(int i=0;i<n;i++)
+        {
+            s1.insert(a[i]);
+        }
+        for(int i=0;i<m;i++)
+        {
+            s1.insert(b[i]);
+        }
+        return s1.size();
+       
+    }
+};
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+} 
