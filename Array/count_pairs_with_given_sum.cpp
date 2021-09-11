cc4f6d8538bfc96bd4cdc05bce0e943763f5ba36
JASWANTH REDDY .P <77910115+jaswanth-0821@users.noreply.github.com>
2021-09-11 16:16:13 +0530

    Update count_pairs_with_given_sum.cpp

--- Array/count_pairs_with_given_sum.cpp
+++ Array/count_pairs_with_given_sum.cpp
@@ -1,5 +1,7 @@
 // Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
 
+// the below solution is from geeks for geeks .
+
 #include <bits/stdc++.h>
 using namespace std;
 
