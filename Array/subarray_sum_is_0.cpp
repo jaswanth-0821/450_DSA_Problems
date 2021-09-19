7f2948d58dc913e2ad18f0fe728874022994ae53
JASWANTH REDDY .P <77910115+jaswanth-0821@users.noreply.github.com>
2021-09-19 21:55:41 +0530

    Create subarray_sum_is_0.cpp

---
+++ Array/subarray_sum_is_0.cpp
@@ -0,0 +1,53 @@
+
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    bool subArrayExists(int arr[], int n)
+    {
+        class Solution{
+  public:
+    bool subArrayExists(int arr[], int n)
+    {
+    	unordered_map<int,bool> sumMap;
+    
+    	int sum = 0;
+    	for (int i = 0 ; i < n ; i++)
+    	{
+    		sum += arr[i];
+    		if (sum == 0 || sumMap[sum] == true)
+    			return true;
+    		sumMap[sum] = true;
+    	}
+    	return false;
+    }
+
+};
+
+    }
+};
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}  
+
+// if anyone finds a better approach please raise a issue by sharing your solution it will help me to improve my understandings.
